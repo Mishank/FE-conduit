210e079c884260230c4c91edf2ba6369a4fe6c6c
Misha <Misha140699@mail.ru>
2023-04-10 17:36:10 +0300

    feat: fix some bugs with server

--- medium_clone/src/index.js
+++ medium_clone/src/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Routes from 'routes'
+
+const App = () => {
+  return (
+    <div>
+      <h3>Welcome to hooks</h3>
+
+      <Router>
+        <Routes/>
+      </Router>
+    </div>
+  )
+}
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
