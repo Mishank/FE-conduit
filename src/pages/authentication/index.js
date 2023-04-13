b7bd53269ec6d8966192507c4eaebfd882840579
Misha <Misha140699@mail.ru>
2023-04-13 15:07:20 +0300

    add localstorage hook lesson 19

--- src/pages/authentication/index.js
+++ src/pages/authentication/index.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react'
-import { Link, useFetcher, useParams, useLocation } from 'react-router-dom'
-import { Navigate } from 'react-router-dom'
+import {
+  Link,
+  useFetcher,
+  useParams,
+  useLocation,
+  Navigate,
+} from 'react-router-dom'
 
 import axios from 'axios'
 
 import useFetch from 'hooks/useFetch'
+import useLocalStorage from 'hooks/useLocalStorage'
 
 const Authentication = (props) => {
   const location = useLocation()
@@ -19,8 +25,9 @@ const Authentication = (props) => {
   const [username, setUsername] = useState('')
   const [isSuccessfullSubmit, setIsSuccessfullSubmit] = useState(false)
   const [{ response, isLoading, error }, doFetch] = useFetch(apiUrl)
+  const [token, setToken] = useLocalStorage('token')
 
-  console.log('fff', isLogin)
+  console.log('token', token)
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -37,7 +44,7 @@ const Authentication = (props) => {
     if (!response) {
       return
     }
-    localStorage.setItem('token', response.user.token)
+    setToken(response.user.token)
     setIsSuccessfullSubmit(true)
   }, [response])
 
