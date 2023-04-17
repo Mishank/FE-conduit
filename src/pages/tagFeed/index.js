f31cf08625398cc807054a0e386fb61cf25aa2ae
Misha <Misha140699@mail.ru>
2023-04-17 13:36:43 +0300

    error wiht file tagFeed

---
+++ src/pages/tagFeed/index.js
@@ -0,0 +1,66 @@
+import React, { useEffect, Fragment } from 'react'
+import { useLocation } from 'react-router-dom'
+import queryString from 'query-string'
+
+import Feed from 'components/feed'
+import useFetch from 'hooks/useFetch'
+import Pagination from 'components/pagination'
+import { getPaginator, limit } from 'utils'
+import PopularTags from 'components/popularTags'
+import Loading from 'components/loading'
+import ErrorMessage from 'components/errorMessage'
+import FeedToggler from 'components/feedToggler'
+
+const TagFeed = (props) => {
+  const location = useLocation()
+  const tagName = location.search.slug
+
+  console.log({ tagName })
+  const { offset, currentPage } = getPaginator(location.search)
+
+  const stringifieldParams = queryString.stringify({
+    limit,
+    offset,
+  })
+  const apiUrl = `/articles?${stringifieldParams}`
+  const [{ response, error, isLoading }, doFetch] = useFetch(apiUrl)
+  const url = location.pathname
+
+  useEffect(() => {
+    doFetch()
+  }, [doFetch, currentPage])
+
+  return (
+    <div className="home-page">
+      <div className="banner">
+        <h1>Medium Clone</h1>
+        <p>A place to share knowledge</p>
+      </div>
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-9">
+            <FeedToggler />
+            {isLoading && <Loading />}
+            {error && <ErrorMessage />}
+            {!isLoading && response && (
+              <>
+                <Feed articles={response.articles} />
+                <Pagination
+                  total={response.articlesCount}
+                  limit={limit}
+                  url={url}
+                  currentPage={currentPage}
+                />
+              </>
+            )}
+          </div>
+          <div className="col-md-3">
+            <PopularTags />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default TagFeed
--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ import { Routes as Switch, Route } from 'react-router-dom'
 import GlobalFeed from 'pages/globalFeed'
 import Article from 'pages/article'
 import Authentication from 'pages/authentication'
+import TagFeed from 'pages/tagFeed'
 
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/" element={<GlobalFeed />} />
+      <Route path="/" element={<GlobalFeed />} exact />
+      <Route path="/tags/:slug" element={<TagFeed />} />
       <Route path="/login" element={<Authentication />} />
       <Route path="/register" element={<Authentication />} />
       <Route path="/articles/:slug" element={<Article />} />
