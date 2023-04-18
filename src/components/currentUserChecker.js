dccfe30fa27eceaa6f70a7ee6140e09adf224bd2
Misha <Misha140699@mail.ru>
2023-04-18 11:54:58 +0300

    lesson 33 add Use reducer

--- src/components/currentUserChecker.js
+++ src/components/currentUserChecker.js
@@ -1,41 +1,31 @@
-import { useContext, useEffect } from 'react'
+import { useEffect, useContext } from 'react'
 
 import useFetch from 'hooks/useFetch'
 import { CurrentUserContext } from 'contexts/currentUser'
 import useLocalStorage from 'hooks/useLocalStorage'
 
 const CurrentUserChecker = ({ children }) => {
+  const [, dispatch] = useContext(CurrentUserContext)
   const [{ response }, doFetch] = useFetch('/user')
-  const [, setCurrentUserState] = useContext(CurrentUserContext)
   const [token] = useLocalStorage('token')
 
   useEffect(() => {
     if (!token) {
-      setCurrentUserState((state) => ({
-        ...state,
-        isLoggedIn: false,
-      }))
+      dispatch({ type: 'SET_UNAUTHORIZED' })
       return
     }
+
     doFetch()
-    setCurrentUserState((state) => ({
-      ...state,
-      isLoading: true,
-    }))
-  }, [token, setCurrentUserState])
+    dispatch({ type: 'LOADING' })
+  }, [doFetch, dispatch, token])
 
   useEffect(() => {
     if (!response) {
       return
     }
-    setCurrentUserState((state) => ({
-      ...state,
-      isLoggedIn: true,
-      isLoading: false,
-      currentUser: response.user,
-    }))
-  }, [response, setCurrentUserState, doFetch])
 
+    dispatch({ type: 'SET_AUTHORIZED', payload: response.user })
+  }, [response, dispatch])
   return children
 }
 
